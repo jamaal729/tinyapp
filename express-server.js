d24800065892fcd8c1c756354560fd2415601474
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-28 23:21:41 +0000

    Implemented post request and random strings

--- express-server.js
+++ express-server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -40,9 +43,33 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   // console.log(req.params.shortURL);
   // console.log(urlDatabase[req.params.shortURL]);
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+function generateRandomString() {
+  let randomString = "";
+  let characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  console.log(characters.length + "\n--");
+  for (let i = 0; i < 6; i++) {
+    let rand = Math.floor(Math.random() * 63);
+    if (rand === 0 || rand === 62) console.log(rand);
+    // console.log(rand);
+
+    randomString += characters.charAt(rand);
+  }
+  return randomString;
+}
