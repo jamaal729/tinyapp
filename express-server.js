58854b30e9ab0154bb82af27b1616c9190171e2f
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-28 21:09:01 +0000

    Implemented displaying single url in shortened form

--- express-server.js
+++ express-server.js
@@ -39,3 +39,10 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  // console.log(req.params.shortURL);
+  // console.log(urlDatabase[req.params.shortURL]);
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
