1bc86d0013a5486d353a41cf435ab8746e4bd4e3
Jamaal Ahmed <jamaal729@yahoo.com>
2019-10-28 19:57:51 +0000

    Added and filled out the urls_index.ejs template

--- express-server.js
+++ express-server.js
@@ -1,12 +1,9 @@
-const express = require("express");
-const app = express();
-const PORT = 8080;
-
-app.set("view engine", "ejs");
 
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -37,3 +34,8 @@ app.get("/hello", (req, res) => {
 // app.get("/fetch", (req, res) => {
 //   res.send(`a = ${a}`);
 // });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
